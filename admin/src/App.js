ef628bdd8e70ec7ac60c671d2b8005ae5e549360
Klaenex <hello@cuozzovincenzo.be>
2023-04-01 23:31:09 +0200

    start admin panel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/menu" element={<Menu />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ admin/src/components/Dropdown.js
@@ -0,0 +1,11 @@
+const Dropdown = () => {
+  return (
+    <>
+      <label for="category">Choose a category:</label>
+
+      <select name="category" id="category"></select>
+    </>
+  );
+};
+
+export default Dropdown;
